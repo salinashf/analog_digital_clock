7a690071bcb82c72f185c3679cb30578c6027231
Salinas Flores Henry <salinashf@gmail.com>
2023-08-30 18:51:09 -0500

    changes  deps

--- lib/modules/fancy_button.dart
+++ lib/modules/fancy_button.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:analog_digital_clock/constants/constants.dart';
 //import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:neumorphic_button/neumorphic_button.dart';
 
 class FancyButton extends StatelessWidget {
   final String label;
@@ -14,29 +15,31 @@ class FancyButton extends StatelessWidget {
       required this.gradient});
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onPress,
-      child: Ink(
-        decoration: BoxDecoration(
-          gradient: gradient,
-          borderRadius: const BorderRadius.all(
-            Radius.circular(10.0),
-          ),
+    Size size = MediaQuery.of(context).size;
+    return NeumorphicButton(
+      borderRadius: 12,
+      bottomRightShadowBlurRadius: 15,
+      bottomRightShadowSpreadRadius: 1,
+      borderWidth: 5,
+      backgroundColor: Colors.grey.shade500,
+      topLeftShadowBlurRadius: 15,
+      topLeftShadowSpreadRadius: 1,
+      topLeftShadowColor: Colors.blue,
+      bottomRightShadowColor: Colors.grey.shade500,
+      height: size.width * 0.2,
+      width: size.width * 0.2,
+      padding: const EdgeInsets.all(50),
+      bottomRightOffset: Offset(4, 4),
+      topLeftOffset: Offset(-4, -4),
+      onTap: onPress,
+      child: const Text(
+       "dasdasdasd",
+        style: TextStyle(
+          color: Color(purple),
+          fontFamily: 'Varela',
+          fontWeight: FontWeight.bold,
         ),
-        child: Container(
-          width: 120.0,
-          height: 50.0,
-          alignment: Alignment.center,
-          child: Text(
-            label,
-            style: const TextStyle(
-              color: Color(purple),
-              fontFamily: 'Varela',
-              fontWeight: FontWeight.bold,
-            ),
-            textAlign: TextAlign.center,
-          ),
-        ),
+        textAlign: TextAlign.center,
       ),
     );
   }
